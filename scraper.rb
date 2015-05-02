cf25fc795377d3792f27db9a4adf671068ca3f3b
Luke Bacon <luke@equivalentideas.com>
2015-05-02 15:02:12 +1000

    lazy commit, first version of scraper code

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,41 @@
 # This is a template for a Ruby scraper on Morph (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+require 'scraperwiki'
+require 'mechanize'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+def save_media_release(page)
+  container = page.search('div#content-main')
+
+  title = container.search(:h1)[1].text
+
+  pub_datetime = DateTime.parse(container.children[8].text, '%A, %d %B %Y %l:%M:%S %p')
+
+  # Strip the release header and meta elements leaving just the body
+  container.children[0...9].remove
+
+  media_release = {
+    title: title,
+    pub_datetime: pub_datetime.to_s,
+    body: container.inner_html,
+    url: page.uri.to_s,
+    scraped_datetime: DateTime.now.to_s
+  }
+
+  puts "Saving #{title} - #{pub_datetime.strftime('%Y-%m-%d')}"
+  ScraperWiki.save_sqlite([:url], media_release)
+end
+
+agent = Mechanize.new
+
+index = agent.get('http://www.police.nsw.gov.au/news/media_release_archives')
+
+index.search('#content_div_111604 a').each do |link|
+  if !ScraperWiki.select("url from data where url='#{link.attr(:href)}'").empty?
+    puts "Skipping already saved media release #{link.text} #{link.attr(:href)}"
+  else
+    media_release_page = agent.get(link.attr(:href))
+    save_media_release(media_release_page)
+  end
+end
+
